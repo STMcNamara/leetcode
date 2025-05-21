269b877cd5c20e7cc92b0167c3b0967971398199
Sean McNamara <stmcnamara.dev@gmail.com>
2025-05-21 20:50:05 +0100

    Added memoization to 198 - fast submission

--- 198.py
+++ 198.py
@@ -4,20 +4,26 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         # Position starting at last house
         pos = len(nums)-1
-        result = self.n_pos_max(nums, pos)
+        result = self.n_pos_max(nums, pos, dict())
         return result
 
 
-    def n_pos_max(self, nums, pos):    
+    def n_pos_max(self, nums, pos, memo):    
         # TODO - Works - add in momoization
+        if pos in memo:
+            return memo[pos]
+        
         if pos == 0:
             rob_max = nums[0]
+            memo[0] = rob_max
         elif pos == 1:
             rob_max = max(nums[pos], nums[pos-1])
+            memo[1] = rob_max
         else:
-            rob_max =max(nums[pos]+self.n_pos_max(nums, pos-2), self.n_pos_max(nums, pos-1))
+            rob_max =max(nums[pos]+self.n_pos_max(nums, pos-2, memo), self.n_pos_max(nums, pos-1, memo))
+            memo[pos] = rob_max
         
         return rob_max
     
 solution = Solution()
-print(solution.rob([2,1,2,1000]))
+print(solution.rob([2,7,9,3,1]))
