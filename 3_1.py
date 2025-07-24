d2caaeb2e2f1f91606307766d464387907675516
Sean McNamara <stmcnamara.dev@gmail.com>
2025-07-24 19:44:21 +0100

    Fast solution with lookup table

--- 3_1.py
+++ 3_1.py
@@ -1,34 +1,36 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-       
-        if len(s) == 0:
-            return 0
-       
-        longest_len = 1
-        longest_str = ""
-       
-        window_start_pos = 0
-        current_str_len = 1
+        l_ind = 0
 
-        for s_pos in range(1, len(s)):
-            # Iterate in reverse over sliding window
-            for j in range(s_pos-1, window_start_pos-1, -1):
-                # print("s_pos: " + str(s_pos))
-                # print("window_pos: " + str(j))
+        last_seen_letter = {}
+        longest_str = 0
+        current_str = 0
 
-                if s[j] == s[s_pos]:
-                    current_str_len = s_pos - j
-                    window_start_pos = j + 1
+        for r_ind in range(len(s)):
+            # Check if current letter has been seen
+            if s[r_ind] in last_seen_letter:
+                letter_prev_ind = last_seen_letter[s[r_ind]]
+            else:
+                letter_prev_ind = -1
 
-                else:
-                    current_str_len  = s_pos - window_start_pos + 1
+            if letter_prev_ind < l_ind:
+                current_str = r_ind - l_ind + 1
 
-            if current_str_len > longest_len:
-                longest_len = current_str_len
+            else:
+                current_str = r_ind - letter_prev_ind
+                l_ind  = letter_prev_ind + 1
 
-            # print(current_str_len)    
+            if current_str > longest_str:
+                longest_str = current_str
 
-        return longest_len
+            # Update the last position
+            last_seen_letter[s[r_ind]] = r_ind
+
+        return longest_str
+
+
+
+
 
 solution = Solution()
 print(solution.lengthOfLongestSubstring("dvdf"))
