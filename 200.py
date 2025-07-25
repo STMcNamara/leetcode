0f6d7a07e131b6f992df91ba23e0400b88187c69
Sean McNamara <stmcnamara.dev@gmail.com>
2025-07-25 19:51:37 +0100

    Before change to BFS lib

--- 200.py
+++ 200.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        viewed = []
+        viewed = set()
         
         n = 0
 
@@ -18,7 +18,7 @@ class Solution:
                     pass
                 # Skip unviewed 0 elements and add to seen
                 elif val == "0":
-                    viewed.append((m,n))
+                    viewed.add((m,n))
                 # Otherwise we have found an unseen 1
                 else:
                 # Initialise a queue with the starting node
@@ -28,7 +28,7 @@ class Solution:
                     while queue:
                         # Get newest node and remove from queue and add to seen
                         node = queue.pop(0)
-                        viewed.append(node)
+                        viewed.add(node)
 
                         # Get neighbours to right and down (above and left must've been seen)
                         # If "0" add to seen and move on
@@ -36,15 +36,27 @@ class Solution:
                         neighbours = []
                         # Right neighbour
                         if node[0] != len(row) - 1: # Avoid RHS boundary
-                            neighbours.append((node[0] + 1, node[1]))
+                            rh_neighbour = (node[0] + 1, node[1])
+                            if rh_neighbour not in viewed:
+                                neighbours.append(rh_neighbour)
+                        if node[0] != 0: # Avoid LHS boundary
+                            lh_neighbour = (node[0] - 1, node[1])
+                            if lh_neighbour not in viewed:
+                                neighbours.append(lh_neighbour)
                         if node[1] != len(grid) - 1: # Avoid Bottom boundary
-                            neighbours.append((node[0], node[1] + 1))
+                            down_neighbour = (node[0], node[1] + 1)
+                            if down_neighbour not in viewed:
+                                neighbours.append(down_neighbour)
+                        if node[1] != 0: # Avoid Top boundary
+                            up_neighbour = (node[0], node[1] - 1)
+                            if up_neighbour not in viewed:
+                                neighbours.append(up_neighbour)
 
                         for neighbour in neighbours:
                             if grid[neighbour[1]][neighbour[0]]== "1":
                                 queue.append(neighbour)
                             else:
-                                viewed.append(neighbour)
+                                viewed.add(neighbour)
 
                     island_count += 1
                 # Increment along row
@@ -59,6 +71,8 @@ class Solution:
 if __name__ == "__main__":
     grid1 = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
     grid2 = [["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]
+    grid3 = [["1","1","1"],["0","1","0"],["1","1","1"]]
     s = Solution()
-#    print(s.numIslands(grid1))
+    print(s.numIslands(grid1))
     print(s.numIslands(grid2))
+    print(s.numIslands(grid3))
