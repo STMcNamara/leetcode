c36272867a67b3bc7963bff17a30d3c28c3b971c
Sean McNamara <stmcnamara.dev@gmail.com>
2025-07-25 21:07:07 +0100

    Working solution

--- 733.py
+++ 733.py
@@ -7,6 +7,12 @@ class Solution:
         # Useful values
         rows = len(image)
         cols = len(image[0])
+
+        # Rember original colour
+        org_col = image[sr][sc]
+        
+        # Set starting colour
+        image[sr][sc] = color
         
         # BFS for valus that are the same
         seen = set()
@@ -15,26 +21,36 @@ class Solution:
         
         while queue:
             node = queue.popleft()
+            seen.add(node)
+
             neighbours = []
             # Above
             if node[0] != 0 and (node[0] - 1, node[1]) not in seen:
                 neighbours.append((node[0] - 1, node[1]))
             # Below
-            if node[0] != rows - 1:
+            if node[0] != rows - 1 and (node[0] + 1, node[1]) not in seen:
                 neighbours.append((node[0] + 1, node[1]))
             # LHS
-            if node[1] != 0:
+            if node[1] != 0 and (node[0], node[1] - 1) not in seen:
                 neighbours.append((node[0], node[1] - 1))
             # RHS
-            if node[1] != cols - 1:
+            if node[1] != cols - 1 and (node[0], node[1] + 1) not in seen:
                 neighbours.append((node[0], node[1] + 1))
-            print(neighbours)
-        return
+
+            for neighbour in neighbours:
+                if image[neighbour[0]][neighbour[1]] == org_col:
+                    image[neighbour[0]][neighbour[1]] = color
+                    queue.append(neighbour)
+                else:
+                    seen.add(neighbour)
+
+
+        return image
 
 # Test Cases
 grid1 = [[1,1,1],[1,1,0],[1,0,1]]
-colour1 = 1
+colour1 = 2
 sc = 2
 sr = 0
 s = Solution()
-s.floodFill(grid1, sr, sc, colour1)
+print(s.floodFill(grid1, sr, sc, colour1))
